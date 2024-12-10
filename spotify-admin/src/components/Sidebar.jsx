3d35725237a3fe46ebcb63425d572025c8697bab
mmdanish <mhddanish567@gmail.com>
2024-12-10 16:01:14 +0530

    commit

--- spotify-admin/src/components/Sidebar.jsx
+++ spotify-admin/src/components/Sidebar.jsx
@@ -1,38 +1,56 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='bg-[#003A10] min-h-screen pl-[4vw]'>
-      <img className='mt-5 w-[max(10vw,100px)] hidden sm:block' src={assets.logo} alt="" />
-      <img className='mt-5 w-[max(5vw,40px)] mr-5 sm:hidden block' src={assets.logo_small} alt="" />
+    <div className="bg-[#003A10] min-h-screen pl-[4vw]">
+      <img
+        className="mt-5 w-[max(10vw,100px)] hidden sm:block"
+        src={assets.logo}
+        alt=""
+      />
+      <img
+        className="mt-5 w-[max(5vw,40px)] mr-5 sm:hidden block"
+        src={assets.logo_small}
+        alt=""
+      />
 
-      <div className='flex flex-col gap-5 mt-10'>
-        <NavLink to='/add-song' className='flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
-            <img className='w-5' src={assets.add_song} alt="" />
-            <p className='hidden sm:block'>Add Song</p>
+      <div className="flex flex-col gap-5 mt-10">
+        <NavLink
+          to="/add-song"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+        >
+          <img className="w-5" src={assets.add_song} alt="" />
+          <p className="hidden sm:block">Add Song</p>
         </NavLink>
 
-        <NavLink to='/lsit-song' className='flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
-            <img className='w-5' src={assets.song_icon} alt="" />
-            <p className='hidden sm:block'>List Song</p>
+        <NavLink
+          to="/lsit-song"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+        >
+          <img className="w-5" src={assets.song_icon} alt="" />
+          <p className="hidden sm:block">List Song</p>
         </NavLink>
 
-        <NavLink to='/add-album' className='flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
-            <img className='w-5' src={assets.add_album} alt="" />
-            <p className='hidden sm:block'>Add Album</p>
+        <NavLink
+          to="/add-album"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+        >
+          <img className="w-5" src={assets.add_album} alt="" />
+          <p className="hidden sm:block">Add Album</p>
         </NavLink>
 
-        <NavLink to='/list-album' className='flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
-            <img className='w-5' src={assets.album_icon} alt="" />
-            <p className='hidden sm:block'>List Album</p>
+        <NavLink
+          to="/list-album"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+        >
+          <img className="w-5" src={assets.album_icon} alt="" />
+          <p className="hidden sm:block">List Album</p>
         </NavLink>
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
