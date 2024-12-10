8ad27d0c773cbd529fd324462d99cb93d6b7a6da
mmdanish <mhddanish567@gmail.com>
2024-12-10 10:37:49 +0530

    commit

--- spotify-backend/src/routes/songRoute.js
+++ spotify-backend/src/routes/songRoute.js
@@ -1,4 +1,8 @@
-import { addSong, listSong, removeSong } from "../controllers/songsController.js";
+import {
+  addSong,
+  listSong,
+  removeSong,
+} from "../controllers/songsController.js";
 import express from "express";
 import upload from "../middleware/multer.js";
 
@@ -13,6 +17,6 @@ songRouter.post(
   addSong
 );
 songRouter.get("/list", listSong);
-songRouter.post("/remove", removeSong)
+songRouter.post("/remove", removeSong);
 
 export default songRouter;
