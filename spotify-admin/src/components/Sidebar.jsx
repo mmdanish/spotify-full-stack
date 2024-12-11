1f797de5fd2978d205714ff56d7c532557d11922
mmdanish <mhddanish567@gmail.com>
2024-12-11 09:25:47 +0530

    commit

--- spotify-admin/src/components/Sidebar.jsx
+++ spotify-admin/src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
       <div className="flex flex-col gap-5 mt-10">
         <NavLink
           to="/add-song"
-          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
         >
           <img className="w-5" src={assets.add_song} alt="" />
           <p className="hidden sm:block">Add Song</p>
@@ -27,7 +27,7 @@ const Sidebar = () => {
 
         <NavLink
           to="/lsit-song"
-          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
         >
           <img className="w-5" src={assets.song_icon} alt="" />
           <p className="hidden sm:block">List Song</p>
@@ -35,7 +35,7 @@ const Sidebar = () => {
 
         <NavLink
           to="/add-album"
-          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
         >
           <img className="w-5" src={assets.add_album} alt="" />
           <p className="hidden sm:block">Add Album</p>
@@ -43,7 +43,7 @@ const Sidebar = () => {
 
         <NavLink
           to="/list-album"
-          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[8vw] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
+          className="flex items-center gap-2.5 bg-white text-gray-800 border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium"
         >
           <img className="w-5" src={assets.album_icon} alt="" />
           <p className="hidden sm:block">List Album</p>
--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const AddSong = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default AddSong
+    const [image, setImage] = useState(false);
+    const [song, setSong] = useState(false);
+    const [name, setName] = useState("");
+    const [desc, setDesc] = useState("");
+    const [album, setAlbum] = useState("none");
+    const [loading, setLoading] = useState(false)
+    const [albumData, setAlbumData] = useState([]);
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+    }
+
+  return (
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-start gap-8 text-gray-600">
+      <div className="flex gap-8">
+        <div className="flex flex-col gap-4">
+          <p>Upload Song</p>
+          <input onChange={(e) => setSong(e.target.files[0])} type="file" id="song" accept="audio/*" hidden />
+          <label htmlFor="song">
+            <img
+              className="w-24 cursor-pointer"
+              src={song ? assets.upload_added : assets.upload_song}
+              alt=""
+            />
+          </label>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p>Upload Image</p>
+          <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" accept="image/*" hidden />
+          <label htmlFor="image">
+            <img
+              className="w-24 cursor-pointer"
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
+          </label>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <p>Song name</p>
+        <input type="text" className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]" placeholder="Type Here" required />
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <p>Song description</p>
+        <input type="text" className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]" placeholder="Type Here" required />
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <p>Album</p>
+        <select className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]">
+            <option value="none">None</option>
+        </select>
+      </div>
+
+      <button type="submit" className="text-base bg-black text-white py-2.5 px-14 cursor-pointer">ADD</button>
+    </form>
+  );
+};
+
+export default AddSong;
